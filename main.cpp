9b0e567ecfa1d71fb96311c0667f13f15c7d8b0a
Matt Carroll <oholiab@gmail.com>
2016-05-17 22:43:17 +0100

    Some weird shit

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ class VREventReceiver : public IEventReceiver {
       if(event.EventType == irr::EET_KEY_INPUT_EVENT){
         if(IsModKey(event.KeyInput.Key)){
           if(event.KeyInput.PressedDown){
+            //FIXME: shit sometimes gets weird here
             CurrentMod |= mapKeyCode(event.KeyInput.Key);
           } else {
             CurrentMod &= ~mapKeyCode(event.KeyInput.Key);
@@ -73,6 +74,7 @@ int main() {
   bill->setSize(core::dimension2d<f32>(20.0f, 20.0f));
   bill->setID(ID_IsNotPickable); // This ensures that we don't accidentally ray-pick it
 
+  // Set up terminal
   scene::IMeshSceneNode *cube = smgr->addCubeSceneNode(15.0f, 0, -1, core::vector3df(10,-10,10), core::vector3df(0,0,0), core::vector3df(4, 4, 1));
   cube->setMaterialFlag(video::EMF_LIGHTING, true);
   if(cube){
@@ -82,24 +84,48 @@ int main() {
   }
   scene::ITriangleSelector* cubeSelector = cube->getTriangleSelector();
 
-  // Create the level and the collision levelSelector
-  device->getFileSystem()->addFileArchive("models/map-20kdm2.pk3");
-  scene::IAnimatedMesh* levelMesh = smgr->getMesh("20kdm2.bsp");
-  scene::IMeshSceneNode* levelNode = 0;
-  scene::ITriangleSelector* levelSelector = 0;
+  // Add lighting
+  scene::ISceneNode* light = smgr->addLightSceneNode(0, core::vector3df(10,20,20),
+      video::SColorf(1.0f, 0.6f, 0.7f, 1.0f), 800.0f);
+
+  scene::IMeshSceneNode *room = smgr->addCubeSceneNode(15.0f, 0, -1, core::vector3df(10,-10,10), core::vector3df(0,0,0), core::vector3df(30, 30, 30));
+  smgr->getMeshManipulator()->flipSurfaces(room->getMesh());
+  room->setMaterialFlag(video::EMF_LIGHTING, false);
+  room->setMaterialTexture(0, driver->getTexture("media/texture.jpg"));
+  room->setTriangleSelector(smgr->createTriangleSelector( room->getMesh(), room ));
+
+  bool yesLevel = true;
+  int collideablesNumber = 3;
+  scene::ITriangleSelector* collideables[collideablesNumber];
+  collideables[0] = cubeSelector;
+  collideables[1] = room->getTriangleSelector();
+
   scene::IMetaTriangleSelector* cameraCollisionSelector = 
     smgr->createMetaTriangleSelector();
-  if (levelMesh) {
-    levelNode = smgr->addOctreeSceneNode(levelMesh->getMesh(0), 0, IDFlag_IsSolid);
-//      node = smgr->addMeshSceneNode(mesh->getMesh(0));
-    if(levelNode){
-      levelNode->setPosition(core::vector3df(-1350,-130,-1400));
-      levelSelector = smgr->createOctreeTriangleSelector(
-                 levelNode->getMesh(), levelNode, 128);
-      cameraCollisionSelector->addTriangleSelector(levelSelector);
-      cameraCollisionSelector->addTriangleSelector(cubeSelector);
-      levelNode->setTriangleSelector(levelSelector);
+  if(yesLevel){
+    // Create the level and the collision levelSelector
+    device->getFileSystem()->addFileArchive("models/map-20kdm2.pk3");
+    scene::IAnimatedMesh* levelMesh = smgr->getMesh("20kdm2.bsp");
+    scene::IMeshSceneNode* levelNode = 0;
+    scene::ITriangleSelector* levelSelector = 0;
+    if (levelMesh) {
+      levelNode = smgr->addOctreeSceneNode(levelMesh->getMesh(0), 0, IDFlag_IsSolid);
+//        node = smgr->addMeshSceneNode(mesh->getMesh(0));
+      if(levelNode){
+        levelNode->setMaterialFlag(video::EMF_LIGHTING, true);
+        levelNode->setPosition(core::vector3df(-1350,-130,-1400));
+        levelSelector = smgr->createOctreeTriangleSelector(
+                   levelNode->getMesh(), levelNode, 128);
+        collideables[2] = levelSelector;
+        levelNode->setTriangleSelector(levelSelector);
+        levelSelector->drop(); // As soon as we're done with the levelSelector, drop it.
+      }
     }
+  } else {
+    collideablesNumber = 2;
+  }
+  for(int i=0; i<collideablesNumber; i++){
+    cameraCollisionSelector->addTriangleSelector(collideables[i]);
   }
 
   //Create camera and pin it to the floor
@@ -107,20 +133,18 @@ int main() {
   camera->setPosition(core::vector3df(50,50,-60));
 
   //Add collision to the camera
-  if (levelSelector) {
-    scene::ISceneNodeAnimator* levelAnim = smgr->createCollisionResponseAnimator(
-      cameraCollisionSelector, camera, core::vector3df(30,50,30),
-      core::vector3df(0,-10,0), core::vector3df(0,30,0));
-    levelSelector->drop(); // As soon as we're done with the levelSelector, drop it.
-    camera->addAnimator(levelAnim);
-    levelAnim->drop();  // And likewise, drop the animator when we're done referring to it.
-  }
+  scene::ISceneNodeAnimator* levelAnim = smgr->createCollisionResponseAnimator(
+    cameraCollisionSelector, camera, core::vector3df(30,50,30),
+    core::vector3df(0,-10,0), core::vector3df(0,30,0));
+  camera->addAnimator(levelAnim);
+  levelAnim->drop();  // And likewise, drop the animator when we're done referring to it.
 
 
   device->getCursorControl()->setVisible(false);
   //camera->setTarget(cube->getAbsolutePosition());
   int lastFPS = -1;
   video::ITexture * texture;
+  scene::ISceneNode* lastSelectedSceneNode = NULL;
   while(device->run()) {
     device->getVideoDriver()->removeTexture(texture);
     texture = driver->getTexture("/tmp/vrhs/shot.png");
@@ -144,13 +168,16 @@ int main() {
           IDFlag_IsInteractable,
           0);
 
+    if(lastSelectedSceneNode) lastSelectedSceneNode->setMaterialFlag(video::EMF_LIGHTING, true);
     if(selectedSceneNode)
     {
       bill->setPosition(intersection);
       driver->setTransform(video::ETS_WORLD, core::matrix4());
       driver->setMaterial(selectMaterial);
       driver->draw3DTriangle(hitTriangle, video::SColor(0, 255, 0, 0));
-    }
+      selectedSceneNode->setMaterialFlag(video::EMF_LIGHTING, false);
+      lastSelectedSceneNode = selectedSceneNode;
+    } 
 
     smgr->drawAll();
     guienv->drawAll();
