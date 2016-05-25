00975035fe2427e24b36040be9bd7fabfb3b8c7d
Matt Carroll <oholiab@gmail.com>
2016-05-25 11:23:32 +0100

    Seperated some bits out from main

---
+++ src/term.cpp
@@ -0,0 +1,15 @@
+
+#include "term.h"
+
+scene::IMeshSceneNode* addTerminal(scene::ISceneManager* smgr){
+  // Set up terminal
+  scene::IMeshSceneNode *terminal = smgr->addCubeSceneNode(15.0f, 0, IDFlag_IsInteractable, core::vector3df(10,-10,10), core::vector3df(0,0,0), core::vector3df(4, 4, 1));
+  terminal->setMaterialFlag(video::EMF_LIGHTING, false); // because monitors are light sources
+  terminal->setMaterialFlag(video::EMF_NORMALIZE_NORMALS, true);
+  terminal->setTriangleSelector( smgr->createTriangleSelector( terminal->getMesh(), terminal ));
+  scene::SMesh* outline = smgr->getMeshManipulator()->createMeshCopy(terminal->getMesh());
+  smgr->getMeshManipulator()->flipSurfaces(outline);
+  outline->setMaterialFlag(video::EMF_LIGHTING, false);
+  smgr->getMeshManipulator()->recalculateNormals(outline);
+  scene::IMeshSceneNode *outlineNode = smgr->addMeshSceneNode(outline, terminal, ID_IsNotPickable | IDFlag_IsOutline, core::vector3df(0,0,0), core::vector3df(0,0,0), core::vector3df(1.05,1.05,1.05));
+}
---
+++ src/term.h
@@ -0,0 +1,11 @@
+#ifndef IRR_INCLUDE
+#include <irrlicht/irrlicht.h>
+#define IRR_INCLUDE
+#endif
+#ifndef IDS_INCLUDE
+#include "ids.h"
+#define IDS_INCLUDE
+#endif
+
+using namespace irr;
+scene::IMeshSceneNode* addTerminal(scene::ISceneManager* smgr);
